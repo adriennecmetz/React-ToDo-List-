5643b3a052658e12a696332619ca26adffb3d691
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-02-14 09:00:40 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    },
+    
+    {
+      text: 'watch youtube video',
+      isCompleted: false,
+    }    
+    
+
+  ])
+
+  return(
+    <>
+      {todos.map((todo, i) =>
+        <div className="todo" key={i}>{todo.text}</div>)}
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
